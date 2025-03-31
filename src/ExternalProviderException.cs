107450cc391c5c080478a24e2c3642f815aa5053
Dani rzv <danirzv@gmail.com>
2025-03-31 17:01:12 +0330

    Add Basic functionalities

---
+++ src/ExternalProviderException.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace MinimalHttp;
+
+public class ExternalProviderException : Exception
+{
+    public ExternalProviderException(string errorCode, string errorTitle, string errorMessage, HttpStatusCode httpStatusCode, Dictionary<string, string[]> errors)
+    {
+        Code = errorCode;
+        Title = errorTitle;
+        Detail = errorMessage;
+        HttpStatusCode = httpStatusCode;
+        Errors = errors;
+
+    }
+
+    public string Code { get; private set; }
+
+    public string Title { get; private set; }
+
+    public string Detail { get; private set; }
+
+    public HttpStatusCode HttpStatusCode { get; private set; }
+
+    public Dictionary<string, string[]> Errors { get; private set; }
+}
+
+public class ExternalProviderException<TErrorModel> : ExternalProviderException
+{
+    public ExternalProviderException(string errorCode,string errorTitle, string errorMessage, HttpStatusCode httpStatusCode, Dictionary<string, string[]> errors, TErrorModel? errorModel) : base(errorCode, errorTitle, errorMessage, httpStatusCode, errors)
+    {
+        ErrorModel = errorModel;
+    }
+
+    public TErrorModel? ErrorModel { get; private set; }
+}
---
+++ src/HttpClientLoggerOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace MinimalHttp;
+
+public class HttpClientLoggerOptions
+{
+    public bool RequestBody { get; set; }
+    public bool ResponseBody { get; set; }
+}
