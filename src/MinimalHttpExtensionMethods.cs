9a6f5ff105d23d44466a18d82ed3a0f41599e652
Dani rzv <danirzv@gmail.com>
2025-07-14 22:30:18 +0330

    Support configuring HttpClient and HttpClientHandler

--- src/MinimalHttpExtensionMethods.cs
+++ src/MinimalHttpExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MinimalHttp;
 
@@ -13,15 +14,103 @@ public static class MinimalHttpExtensionMethods
     /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
     /// </summary>
     /// <param name="services"></param>
-    /// <typeparam name="TClient"></typeparam>
-    /// <typeparam name="TOptions"></typeparam>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your HttpClient</typeparam>
     /// <returns></returns>
     public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services)
         where TClient : class
         where TOptions : HttpClientOptions
     {
         return services.AddHttpClient<TClient>()
-            .ConfigureHttpClient((sp, httpClient) =>
+            .SetupDefaultFunctionality<TOptions>();
+    }
+
+    /// <summary>
+    /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureClientHandler">which will be used to configure your <see cref="T:System.Net.Http.HttpClientHandler" /></param>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your <see cref="T:System.Net.Http.HttpClient" /></typeparam>
+    /// <returns></returns>
+    public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services, Action<HttpClientHandler> configureClientHandler)
+        where TClient : class
+        where TOptions : HttpClientOptions
+    {
+        return services.AddHttpClient<TClient>()
+            .SetupDefaultFunctionality<TOptions>(configureClientHandler);
+    }
+
+    /// <summary>
+    /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureClient">A delegate that is used to configure an <see cref="T:System.Net.Http.HttpClient" />.</param>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your <see cref="T:System.Net.Http.HttpClient" /></typeparam>
+    /// <returns></returns>
+    public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services, Action<HttpClient> configureClient)
+        where TClient : class
+        where TOptions : HttpClientOptions
+    {
+        return services.AddHttpClient<TClient>(configureClient)
+            .SetupDefaultFunctionality<TOptions>();
+    }
+
+    /// <summary>
+    /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureClient">A delegate that is used to configure an <see cref="T:System.Net.Http.HttpClient" />.</param>
+    /// <param name="configureClientHandler">which will be used to configure your <see cref="T:System.Net.Http.HttpClientHandler" /></param>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your <see cref="T:System.Net.Http.HttpClient" /></typeparam>
+    /// <returns></returns>
+    public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services, Action<HttpClient> configureClient, Action<HttpClientHandler> configureClientHandler)
+        where TClient : class
+        where TOptions : HttpClientOptions
+    {
+        return services.AddHttpClient<TClient>(configureClient)
+            .SetupDefaultFunctionality<TOptions>(configureClientHandler);
+    }
+    
+    /// <summary>
+    /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureClient">A delegate that is used to configure an <see cref="T:System.Net.Http.HttpClient" />.</param>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your <see cref="T:System.Net.Http.HttpClient" /></typeparam>
+    /// <returns></returns>
+    public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services, Action<IServiceProvider, HttpClient> configureClient)
+        where TClient : class
+        where TOptions : HttpClientOptions
+    {
+        return services.AddHttpClient<TClient>(configureClient)
+            .SetupDefaultFunctionality<TOptions>();
+    }
+
+    /// <summary>
+    /// Registers <see cref="T:System.Net.Http.HttpClient" /> with options presented in TOptions and name of TClient type
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configureClient">A delegate that is used to configure an <see cref="T:System.Net.Http.HttpClient" />.</param>
+    /// <param name="configureClientHandler">which will be used to configure your <see cref="T:System.Net.Http.HttpClientHandler" /></param>
+    /// <typeparam name="TClient">The class which named <see cref="T:System.Net.Http.HttpClient" /> will be assigned to, make sure to register it before this method call</typeparam>
+    /// <typeparam name="TOptions">The <see cref="MinimalHttp.HttpClientOptions"/> which will be used to configure your <see cref="T:System.Net.Http.HttpClient" /></typeparam>
+    /// <returns></returns>
+    public static IHttpClientBuilder AddHttpClient<TClient, TOptions>(this IServiceCollection services, Action<IServiceProvider, HttpClient> configureClient, Action<HttpClientHandler> configureClientHandler)
+        where TClient : class
+        where TOptions : HttpClientOptions
+    {
+        return services.AddHttpClient<TClient>(configureClient)
+            .SetupDefaultFunctionality<TOptions>(configureClientHandler);
+    }
+
+    private static IHttpClientBuilder SetupDefaultFunctionality<TOptions>(this IHttpClientBuilder builder, Action<HttpClientHandler>? configureClientHandler = null)
+        where TOptions : HttpClientOptions
+    {
+        return builder.ConfigureHttpClient((sp, httpClient) =>
             {
                 var options = sp.GetService<IOptionsMonitor<TOptions>>()!.CurrentValue;
 
@@ -34,9 +123,17 @@ public static class MinimalHttpExtensionMethods
 
                 var realHandler = new HttpClientHandler();
 
-                realHandler.UseProxy = false;
-                realHandler.AllowAutoRedirect = false;
-                realHandler.UseCookies = false;
+                if (configureClientHandler is not null)
+                {
+                    configureClientHandler.Invoke(realHandler);
+                }
+                else
+                {
+                    realHandler.UseProxy = false;
+                    realHandler.AllowAutoRedirect = false;
+                    realHandler.UseCookies = false;
+                }
+
                 realHandler.ServerCertificateCustomValidationCallback = options.CertificateValidationCallback!;
 
                 var loggingWrapper = new HttpClientLogger(
